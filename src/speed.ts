ed73323a35a957c78719166a81e30e3da17ad785
ahaoboy <504595380@qq.com>
2024-05-22 16:18:29 +0800

    speed: add delay

--- src/speed.ts
+++ src/speed.ts
@@ -1,27 +1,37 @@
 import { PropertyNumber, addKeyBinding } from "@mpv-easy/tool";
 
 const speed = new PropertyNumber("speed");
+
+let mpvSpeed = speed.value;
+let handle = 0;
+const delay = 200
 addKeyBinding(
-	"MOUSE_BTN0",
-	"MPV_EASY_SPEED",
-	({ event }) => {
-		switch (event) {
-			case "down": {
-				speed.value = 2;
-				break;
-			}
-			case "up": {
-				speed.value = 1;
-				break;
-			}
-			case "press": {
-				break;
-			}
-		}
-	},
-	{
-		complex: true,
-		repeatable: true,
-		forced: false,
-	},
+  "MOUSE_BTN0",
+  "MPV_EASY_SPEED",
+  ({ event }) => {
+    switch (event) {
+      case "down": {
+        mpvSpeed = speed.value
+
+        handle = +setTimeout(() => {
+          speed.value = mpvSpeed * 2;
+        }, delay);
+
+        break;
+      }
+      case "up": {
+        speed.value = mpvSpeed
+        clearTimeout(handle)
+        break;
+      }
+      case "press": {
+        break;
+      }
+    }
+  },
+  {
+    complex: true,
+    repeatable: true,
+    forced: false,
+  },
 );
