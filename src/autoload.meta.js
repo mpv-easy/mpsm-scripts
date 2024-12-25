b46aeeee907199049473ac631179eaa51a371307
ahaoboy <504595380@qq.com>
2024-12-25 14:46:50 +0800

    update json

--- src/autoload.meta.js
+++
@@ -1,7 +0,0 @@
-// ==UserScript==
-// @name         autoload
-// @version      0.1.1
-// @description  Automatically load playable files
-// @author       mpv-easy
-// @downloadURL  https://github.com/mpv-easy/mpsm-scripts/releases/latest/download/autoload.js
-// ==/UserScript==
