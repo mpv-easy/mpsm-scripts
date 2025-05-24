bbe214c357bed387c272d7d196be49733339e994
阿豪 <504595380@qq.com>
2025-05-24 09:55:48 +0800

    Add more meta (#1)
    * add meta
    
    * add meta
    
    * add meta
    
    * add meta
    
    * add meta

---
+++ build/merge.ts
@@ -0,0 +1,17 @@
+import { writeFileSync, readFileSync, readdirSync } from "node:fs"
+import { join } from "node:path"
+import { getMetaByLang, Meta } from "@mpv-easy/mpsm"
+
+const META_DIR = "meta"
+
+const v: Record<string, Meta> = {}
+
+for (const i of readdirSync(META_DIR)) {
+  const p = join(META_DIR, i)
+  const s = readFileSync(p, "utf8")
+  const ext = i.endsWith('.js') ? 'js' : 'lua'
+  const json = getMetaByLang(s, ext)!
+  v[i.replace(`.meta.${ext}`, "")] = json
+}
+
+writeFileSync("./meta.json", JSON.stringify(v))
